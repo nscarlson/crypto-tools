3f0a555f8444a264f7b370f63ec38fce7a752187
Nathan Carlson <nathan.s.carlson@gmail.com>
2017-10-22 20:08:59 -0600

    Remove 'children' propType from Body

--- src/services/server/services/Render/components/Document/components/Body/Body.js
+++ src/services/server/services/Render/components/Document/components/Body/Body.js
@@ -1,12 +1,11 @@
 import fs from 'fs'
-import { string } from 'prop-types'
 import React from 'react'
 
 import ClientScript from './components/ClientScript'
 
 let assets
 
-const Body = ({ children }) => {
+const Body = () => {
   if (!assets) {
     assets = JSON.parse(fs.readFileSync('webpack-assets.json'))
   }
@@ -15,7 +14,7 @@ const Body = ({ children }) => {
 
   return (
     <body>
-      <div dangerouslySetInnerHTML={{ __html: children }} id="app" />
+      <div id="app" />
       <ClientScript src={manifest.js} />
       <ClientScript src={vendor.js} />
       <ClientScript src={client.js} />
@@ -24,9 +23,5 @@ const Body = ({ children }) => {
 }
 
 Body.displayName = 'Body'
-
-Body.propTypes = {
-  children: string.isRequired,
-}
 
 export default Body
