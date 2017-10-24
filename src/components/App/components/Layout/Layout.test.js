a6dce26b5b52e4a4c3c4245ea104ce275900f7f7
Nathan Carlson <nathan.s.carlson@gmail.com>
2017-10-23 23:46:57 -0600

    Remove react-grid-layout and update test

--- src/components/App/components/Layout/Layout.test.js
+++ src/components/App/components/Layout/Layout.test.js
@@ -3,19 +3,11 @@ import React from 'react'
 import toJson from 'enzyme-to-json'
 
 import Layout from './Layout'
-import BestRates from './components/Grid/components/BestRates'
-import Explore from './components/Grid/components/Explore'
-import Price from './components/Grid/components/Price'
-
-jest.mock('react-grid-layout')
 
 describe('<Layout />', () => {
-  let ReactGridLayout, wrapper
+  let wrapper
 
   beforeEach(() => {
-    ReactGridLayout = require('react-grid-layout')
-    ReactGridLayout.WidthProvider.mockImplementation((RGL) => {})
-
     wrapper = shallow(<Layout />)
   })
 
